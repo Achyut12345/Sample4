8964860843d67823cbb553b8146de5ec3e1560f4
ac.wadghane <ac.wadghane@gmail.com>
2017-08-04 16:08:36 +0530

    Added UnitTest6

---
+++ SampleDemo/SampleDemo/UnitTest6.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleDemo
+{
+    [TestClass]
+    public class UnitTest6
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
