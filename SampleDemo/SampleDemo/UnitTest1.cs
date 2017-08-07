42e3092e88e309ab79c14544713693c6bacb6f99
ac.wadghane <ac.wadghane@gmail.com>
2017-08-07 10:24:13 +0530

    Added test scenario

--- SampleDemo/SampleDemo/UnitTest1.cs
+++ SampleDemo/SampleDemo/UnitTest1.cs
@@ -14,7 +14,15 @@ namespace SampleDemo
         public void TestMethod1()
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("www.google.com");
+            driver.Navigate().GoToUrl("https://darent82.crm.dynamics.com/main.aspx");
+
+            driver.FindElement(By.Id("cred_userid_inputtext")).Click();
+            driver.FindElement(By.Id("cred_userid_inputtext")).SendKeys("easyrepro@pfecrmonline.onmicrosoft.com");
+            driver.FindElement(By.Id("cred_userid_inputtext")).SendKeys(Keys.Tab);
+
+            driver.FindElement(By.Id("cred_password_inputtext")).SendKeys("R@ngers2017");
+            driver.FindElement(By.Id("cred_password_inputtext")).SendKeys(Keys.Tab);
+            driver.FindElement(By.Id("cred_password_inputtext")).Submit();
             Thread.Sleep(3000);
         }
     }
