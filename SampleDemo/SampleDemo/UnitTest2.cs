bbeec6f5a6f1feb1f2a191d6ca6dbfd85bd79be9
ac.wadghane <ac.wadghane@gmail.com>
2017-08-10 15:28:22 +0530

    updated Unittest2

--- SampleDemo/SampleDemo/UnitTest2.cs
+++ SampleDemo/SampleDemo/UnitTest2.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace SampleDemo
 {
@@ -9,6 +12,17 @@ namespace SampleDemo
         [TestMethod]
         public void TestMethod1()
         {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://darent82.crm.dynamics.com/main.aspx");
+
+            driver.FindElement(By.Id("cred_userid_inputtext")).Click();
+            driver.FindElement(By.Id("cred_userid_inputtext")).SendKeys("easyrepro@pfecrmonline.onmicrosoft.com");
+            driver.FindElement(By.Id("cred_userid_inputtext")).SendKeys(Keys.Tab);
+
+            driver.FindElement(By.Id("cred_password_inputtext")).SendKeys("R@ngers2017");
+            driver.FindElement(By.Id("cred_password_inputtext")).SendKeys(Keys.Tab);
+            driver.FindElement(By.Id("cred_password_inputtext")).Submit();
+            Thread.Sleep(3000);
         }
     }
 }
